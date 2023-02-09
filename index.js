5e75d5e8e28a4c70992f5591defd235e5c19e661
kirubel demelash <62418642+kiztopia1@users.noreply.github.com>
2023-02-09 11:25:23 +0300

    path added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var path = require('path');
 const port = 3000;
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
